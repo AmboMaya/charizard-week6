d57d26bd20564c1dea5ff5acb8072e896f85c751
Ice Maya <echaymaya@gmail.com>
2018-11-09 23:05:19 +1300

    start up pack for personalProject

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a react template with full node.js server using express.js.
+
+Webpack 4 is set up and configured with a dev and production version. 
+
+To Run:
+
+git clone 
+
+yarn 
+
+yarn dev or yarn prod for different env's
---
+++ client/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className = 'page'>
+        <p>React Template is up and Running!</p>
+      </div>
+    )
+  }
+}
+
+export default App
