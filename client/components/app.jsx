9448539c18eaccba15d7015b49e685d45786112e
Ice Maya <echaymaya@gmail.com>
2018-11-11 19:32:00 +1300

    modified App.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import Header from './Layouts/Header'
 import Footer from './Layouts/Footer'
 import Prices from './Layouts/Prices'
+import Map from './Layouts/Map'
+
 
 class App extends React.Component {
   render () {
@@ -9,6 +11,7 @@ class App extends React.Component {
       <Fragment>
         <Header />
         <Prices />
+        <Map />
         <Footer />
       </Fragment>
     )
