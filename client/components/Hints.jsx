97e0dc2e755f253bf1f55405f7c59566ad6e56ea
Alina <alna.kireeva88@gmail.com>
2018-11-23 12:31:30 +1300

    populating the Hints component

--- client/components/Hints.jsx
+++ client/components/Hints.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 export class Hints extends Component {
+  constructor(props)
+  super(props) {
+    this.state = {
+      hint1: "this is hint 1",
+      hint2: "this is hint 2",
+      hint3: "this is hint 3",
+      hint4: "this is hint 4"
+    }
+  }
   render() {
     return (
       <div>
         this is the hint component
+        <div className='Hint'>placeholder: Hint1</div>
+        <div className='Hint'></div>
+        <div className='Hint'></div>
+        <div className='Hint'></div>
       </div>
     )
   }
