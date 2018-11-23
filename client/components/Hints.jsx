f7491324df322acdc76c6d76f4671b3aab7a64fc
Alina <alna.kireeva88@gmail.com>
2018-11-23 14:26:02 +1300

    added a timer in a Hint component

--- client/components/Hints.jsx
+++ client/components/Hints.jsx
@@ -4,23 +4,25 @@ export class Hints extends Component {
   constructor(props) {
   super(props) 
     this.state = {
-      hints: ['this is hint 1', 'this is hint 2', 'this is hint 3', 'this is hint 4',]
+      hints: ['this is hint 1', 'this is hint 2', 'this is hint 3', 'this is hint 4',],
+      hint:'test',
+      counter: 0
     }
+    this.clickHandler = this.clickHandler.bind(this)
+    this.showHints = this.showHints.bind(this)
   }
 
-  clickHandler () {
-    let timer = setInterval(showHints, 10000)
-  
-    let counter = 0
-
-  }
-    showHints = () => {
-    element.innerHTML= hints[counter]
-    counter ++
-    if (counter >= this.state.hints.length) {
-      counter = 0
+    showHints () {
+    this.setState({hint: this.state.hints[this.state.counter], 
+    counter: this.state.counter +1})
+    if (this.state.counter >= this.state.hints.length) {
+      //somethign
     }
   }
+    clickHandler () {
+    console.log("button")
+    let timer = setInterval(this.showHints, 1000)
+    }
   render () {
     return (
       <div>
@@ -28,6 +30,7 @@ export class Hints extends Component {
       clickme
       </button>
         this is the hint component
+        <h3>{this.state.hint}</h3>
         <div className='hint'>{this.state.hints[0]}</div>
         <div className='hint'>{this.state.hints[1]}</div>
         <div className='hint'>{this.state.hints[2]}</div>
