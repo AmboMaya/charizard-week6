c655d6ac26a69a7387ade2782fa1099072d89bb3
Ice Maya <echaymaya@gmail.com>
2018-11-23 15:52:15 +1300

    moved function form hints to main

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -3,7 +3,9 @@ import Hints from './Hints'
 import Map from './Map'
 
 export class Main extends Component {
-  state ={
+  constructor(props) {
+  super(props)
+  this.state = {
     question1: {
       hints: ['Entertainment', 'EDAs hidden talents','Alcohol required', 'Better with Allies rap'],
       position: {
@@ -11,29 +13,60 @@ export class Main extends Component {
         lng: 174.7583916
       },
       answer : 'Happy KTV',
-      show: false
+      
+      show: false,
+      
     },
-    question1: {
+    question2: {
       hints: ['', 'EDAs hidden talents','Alcohol required', 'Better with Allies rap'],
       position: {
         lat: -36.880496,
         lng: 174.795649
       },
       answer : 'EDA',
+      counter: 1,
       show: false
+    },
+    counter: 1
+  }
+  this.clickHandler = this.clickHandler.bind(this)
+  this.showHints = this.showHints.bind(this)
+  this.changeHandler = this.changeHandler.bind(this)
+  }
+  
+ 
+  showHints () {
+    this.setState({hint: this.state.question1.hints[this.state.counter], 
+    counter: this.state.counter +1})
+    if (this.state.counter >= this.state.question1.hints.length) {
+      // clearInterval()
+      this.setState({
+        question1: {
+          show: true
+        }
+      })
     }
   }
+    clickHandler () {
+    this.setState({hint:this.state.question1.hints[0]})
+    let timer = setInterval(this.showHints, 3000)
+    }
+
+    changeHandler (e) {
+      this.setState({answer: e.target.value})
+    }
   render() {
     return (
       <React.Fragment>
         This main Page
-        <Hints />
         <div className="row">
           <div className='col s12 m7'>
             <Map question={this.state.question1}/>
           </div>
           <div className='col s12 m5'>
-            {/* <Map /> change this to hints */}
+            <Hints  clickHandler={this.clickHandler} 
+                    changeHandler={this.changeHandler}
+                    hint={this.state.hint}/>
           </div>
         </div>
       </React.Fragment>
