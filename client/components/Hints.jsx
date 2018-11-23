c4942935d7c55583af00f14a1e76b4a601295fc2
Alina <alna.kireeva88@gmail.com>
2018-11-23 13:08:11 +1300

    trying to make those hint working in the Hint component

--- client/components/Hints.jsx
+++ client/components/Hints.jsx
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
 
 export class Hints extends Component {
-  constructor(props)
-  super(props) {
+  constructor(props) {
+  super(props) 
     this.state = {
-      hint1: "this is hint 1",
-      hint2: "this is hint 2",
-      hint3: "this is hint 3",
-      hint4: "this is hint 4"
+      hints: ['this is hint 1', 'this is hint 2', 'this is hint 3', 'this is hint 4',]
     }
   }
-  render() {
+
+  clickHandler () {
+    let timer = setInterval(showHints, 10000)
+  
+    let counter = 0
+
+  }
+    showHints = () => {
+    element.innerHTML= hints[counter]
+    counter ++
+    if (counter >= this.state.hints.length) {
+      counter = 0
+    }
+  }
+  render () {
     return (
       <div>
+      <button onClick={this.clickHandler}>
+      clickme
+      </button>
         this is the hint component
-        <div className='Hint'>placeholder: Hint1</div>
-        <div className='Hint'></div>
-        <div className='Hint'></div>
-        <div className='Hint'></div>
+        <div className='hint'>{this.state.hints[0]}</div>
+        <div className='hint'>{this.state.hints[1]}</div>
+        <div className='hint'>{this.state.hints[2]}</div>
+        <div className='hint'>{this.state.hints[3]}</div>
       </div>
     )
   }
