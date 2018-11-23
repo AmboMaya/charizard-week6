b74809d37d027aa13e2d89fb4614c769afe2fd1e
Ice Maya <echaymaya@gmail.com>
2018-11-23 14:57:22 +1300

    map is working

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -3,12 +3,39 @@ import Hints from './Hints'
 import Map from './Map'
 
 export class Main extends Component {
+  state ={
+    question1: {
+      hints: ['Entertainment', 'EDAs hidden talents','Alcohol required', 'Better with Allies rap'],
+      position: {
+        lat: -36.8537569,
+        lng: 174.7583916
+      },
+      answer : 'Happy KTV',
+      show: false
+    },
+    question1: {
+      hints: ['', 'EDAs hidden talents','Alcohol required', 'Better with Allies rap'],
+      position: {
+        lat: -36.880496,
+        lng: 174.795649
+      },
+      answer : 'EDA',
+      show: false
+    }
+  }
   render() {
     return (
       <React.Fragment>
         This main Page
         <Hints />
-        <Map />
+        <div className="row">
+          <div className='col s12 m7'>
+            <Map question={this.state.question1}/>
+          </div>
+          <div className='col s12 m5'>
+            {/* <Map /> change this to hints */}
+          </div>
+        </div>
       </React.Fragment>
       
     )
